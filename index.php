67f267ecd86f3fc3833e99624b3485b7fbed7f73
Uwe Siemer <uwe@scobiform.com>
2022-08-03 10:26:29 +0200

    Working callback

--- index.php
+++ index.php
@@ -91,6 +91,8 @@
 	
                             $access_token = $_SESSION['access_token'];
                             $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+                            $user = $connection->get('account/verify_credentials', ['tweet_mode' => 'extended', 'include_entities' => 'true']);
                         }
                     ?>
 
