2cf43a0c90ba03e1518316aa4b5d5db6d96f88c6
Uwe Siemer <uwe@scobiform.com>
2022-08-04 15:15:02 +0200

    Introducing functions.php

---
+++ funxtions.php
@@ -0,0 +1,30 @@
+<?php
+    // Including functions to manage Twitter APIv2 from client account
+
+
+    function GetUserByName()
+    {
+
+    }
+
+    function GetUserById()
+    {
+
+    }
+
+    function GetUserFriends()
+    {
+
+    }
+
+    function GetUserFollowers()
+    {
+
+    }
+
+
+
+
+
+
+?>
